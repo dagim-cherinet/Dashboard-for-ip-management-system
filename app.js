7ced25580ddda8352cf7ff14dde795910c765420
Dagim Cherinet <80765887+dagim-cherinet@users.noreply.github.com>
2022-02-17 19:32:55 +0300

    added change password to myprofile tab

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const app = express();
 const networks = require("./routes/Networks");
 const connectDB = require("./db/connect");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { NewNetwork, User } = require("./models/Networks");
+const JWT_STRING = "kfkjfkjfdkjakdjferuej#$#$#2u3@#@$@kfj";
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -15,6 +19,25 @@ app.use(express.json());
 // routes
 
 app.use("/api", networks);
+app.use("/api/users/change-password", async (req, res) => {
+  const { token, password } = req.body;
+
+  try {
+    const user = jwt.verify(token, JWT_STRING);
+    // console.log(user);
+    const newPassword = await bcrypt.hash(password, 10);
+
+    await User.updateOne(
+      { _id: user._id },
+      {
+        $set: { password: newPassword },
+      }
+    );
+    return res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
